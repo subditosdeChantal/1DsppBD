89058931885e64d9d0493ad77bdb504f5970544e
subditosdeChantal <subditosdeChantal@gmail.com>
2019-11-12 11:54:36 +0100

    Added bash script to run full analysis

--- analysis/README.md
+++ analysis/README.md
@@ -1,1 +1,7 @@
+# Carpeta de análisis
+
 Esta carpeta contiene códigos de análisis de datos: plots, etc. Se corren desde aquí y funcionan con todas las simulaciones.
+
+1. Se pueden correr individualmente con gnuplot, ejemplo: gnuplot plot_nclusters
+
+1. O se pueden correr todos juntos: bash run_analysis.sh
---
+++ analysis/run_analysis.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Plotting cluster size distributions..."
+gnuplot plot_size_distr
+gnuplot plot_size_distr_norm
+
+echo "Plotting direction distributions..."
+gnuplot plot_dir_distr
+
+echo "Plotting total number of clusters evolution..."
+gnuplot plot_nclusters
+
+echo "Plotting system snapshots..."
+gnuplot plot_positions
+gnuplot plot_positions_directions
+
+echo "Done!"
