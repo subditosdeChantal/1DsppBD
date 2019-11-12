9c2d134f024ce3c7c1abf644f1629482c55f9527
subditosdeChantal <subditosdeChantal@gmail.com>
2019-11-12 14:08:37 +0100

    Added bash script to launch simulation and analysis together

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Launching simulation loop..."
+cd codes/
+./BD
+echo "Simulation's over"
+cd ../analysis/
+echo ""
+echo "Launching analysis..."
+echo ""
+bash run_analysis.sh
+cd
+
