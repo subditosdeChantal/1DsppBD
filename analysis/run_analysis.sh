11681a1e3cd6c0e7c86ea311ae833e090533b2bd
subditosdeChantal <subditosdeChantal@gmail.com>
2019-11-18 15:52:52 +0100

    Now bash script in analysis runs as well

--- analysis/run_analysis.sh
+++ analysis/run_analysis.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 echo "Plotting cluster size distributions..."
-gnuplot plot_size_distr -c 'sim*'
-gnuplot plot_size_distr_norm -c 'sim*'
+gnuplot -c plot_size_distr $1
+gnuplot -c plot_size_distr_norm $1
 
 echo "Plotting direction distributions..."
-gnuplot plot_dir_distr -c 'sim*'
+gnuplot -c plot_dir_distr $1
 
 echo "Plotting total number of clusters evolution..."
-gnuplot plot_nclusters -c 'sim*'
+gnuplot -c plot_nclusters $1
 
 echo "Plotting correlations..."
-python3 correlations.py 'sim*'
-gnuplot plot_correlations -c 'sim*'
+python3 correlations.py $1
+gnuplot -c plot_correlations $1
 
 echo "Plotting system snapshots..."
-gnuplot plot_snapshot_beg -c 'sim*'
-gnuplot plot_snapshot_ss -c 'sim*'
+gnuplot -c plot_snapshot_beg $1
+gnuplot -c plot_snapshot_ss $1
 
 echo "Done!"
